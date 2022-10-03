a87732fda49226c1b7d4d73195b5a462e5753e3d
Balogun Razak <razak2free@gmail.com>
2022-10-03 20:41:39 +0100

    03/09/22

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -7,7 +7,6 @@ const Container = styled.div`
 width: 100%;
 height: 100vh;
 display: flex;
-background-color: coral;
 position : relative;
 overflow: hidden;
 `; 
@@ -19,13 +18,39 @@ const Wrapper = styled.div `
 const Slide = styled.div`
 display: flex;
 align-items: center;
+width: 100vw;
+height: 100vh;
 `;
+
+
+const Image = styled.img `
+  height: 80%;
+`
 const ImgContainer = styled.div`
+height: 100%;
+
 flex: 1;
 `;
 const InfoContainer = styled.div`
 flex: 1;
+padding: 50px;
 `;
+
+const Title = styled.h1 `
+font-size: 70px;
+`
+const Desc = styled.p `
+margin: 50px 0;
+font-size: 20px;
+font-weight: 500px;
+letter-spacing: 3px;
+`
+const Button = styled.button `
+font-size: 20px;
+padding: 10px;
+background-color: transparent;
+cursor: pointer;
+`
  
 // for the arrow tag
 const Arrow = styled.div`
@@ -59,7 +84,16 @@ const Slider = () => {
             <ArrowLeftOutlined />
         </Arrow>
         <Wrapper>
-
+          <Slide>
+          <ImgContainer>
+          <Image src="https://i.pinimg.com/564x/ca/44/46/ca4446d969af66b3f02308225ba2386b.jpg"/>
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+            <Button>SHOP NOW</Button>        
+          </InfoContainer>
+          </Slide>
         </Wrapper>
         <Arrow  direction = 'right'> 
             <ArrowRightOutlined />
